ffab9e0671e02b5ff1f0436c0235d06a1c7c83ce
zellytozelly <zellytozelly@gmail.com>
2022-10-23 14:31:55 +0900

    Feat: Add bmi range color

--- components/bmi/Result.tsx
+++ components/bmi/Result.tsx
@@ -9,8 +9,18 @@ interface Props {
   setIsNext: Dispatch<SetStateAction<boolean>>;
 }
 
+const BMI_COLOR_LIST = [
+  colors.LIGHT_GREY,
+  colors.DARK_YELLOW,
+  colors.DARK_GREEN,
+  colors.DARK_MINT,
+  colors.DARK_BLUE,
+  colors.DARK_PINK,
+  colors.DARK_RED,
+];
+
 const Result = ({ bmi, setIsNext }: Props) => {
-  const rangeText = useMemo(() => {
+  const rangeArray = useMemo(() => {
     return checkBmiRange(bmi);
   }, [bmi]);
 
@@ -23,9 +33,30 @@ const Result = ({ bmi, setIsNext }: Props) => {
       <BmiTextBox>
         <p>나의 BMI 지수</p>
         <p>
-          {rangeText}({bmi})
+          {rangeArray.name}({bmi})
         </p>
       </BmiTextBox>
+
+      <ColorList>
+        <ColorItem colorCode={1} activeCode={rangeArray.id}>
+          0
+        </ColorItem>
+        <ColorItem colorCode={2} activeCode={rangeArray.id}>
+          18.5
+        </ColorItem>
+        <ColorItem colorCode={3} activeCode={rangeArray.id}>
+          23
+        </ColorItem>
+        <ColorItem colorCode={4} activeCode={rangeArray.id}>
+          25
+        </ColorItem>
+        <ColorItem colorCode={5} activeCode={rangeArray.id}>
+          30
+        </ColorItem>
+        <ColorItem colorCode={6} activeCode={rangeArray.id}>
+          35
+        </ColorItem>
+      </ColorList>
 
       <RestartButton type="button" onClick={handleClickRestart}>
         다시하기
@@ -50,6 +81,32 @@ const BmiTextBox = styled.div`
     font-weight: bold;
 
     color: ${colors.TEXT_39};
+  }
+`;
+
+const ColorList = styled.ul`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 50px;
+`;
+
+interface ColorItemProps {
+  colorCode: number;
+  activeCode: number;
+}
+
+const ColorItem = styled.li<ColorItemProps>`
+  width: 50px;
+  height: 14px;
+  padding: 2px 4px;
+  font-size: 12px;
+  color: ${(props) => (props.colorCode === props.activeCode ? colors.WHITE : colors.TEXT_59)};
+  background-color: ${(props) =>
+    props.colorCode === props.activeCode ? BMI_COLOR_LIST[props.colorCode] : BMI_COLOR_LIST[0]};
+
+  & + & {
+    margin-left: 2px;
   }
 `;
 
--- utils/bmi.ts
+++ utils/bmi.ts
@@ -1,8 +1,14 @@
 export const checkBmiRange = (bmi: number) => {
-  const bmiRangeList = ['저체중', '정상', '과체중', '경도 비만', '중등도 비만', '고도 비만'];
-  if (bmi < 0) return;
+  const bmiRangeList = [
+    { id: 1, name: '저체중' },
+    { id: 2, name: '정상' },
+    { id: 3, name: '과체중' },
+    { id: 4, name: '경도 비만' },
+    { id: 5, name: '중등도 비만' },
+    { id: 6, name: '고도 비만' },
+  ];
 
-  if (bmi <= 18.4) return bmiRangeList[0];
+  if (bmi >= 0 && bmi <= 18.4) return bmiRangeList[0];
   else if (bmi <= 22.9) return bmiRangeList[1];
   else if (bmi <= 24.9) return bmiRangeList[2];
   else if (bmi <= 29.9) return bmiRangeList[3];
